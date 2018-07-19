87861e92ef757475cae01435f7f7c71af1184a40
Semper-Viventem <kostyaxxx8@gmail.com>
2018-07-19 22:05:39 +0300

    Hide keyboard on close

--- app/src/main/java/ru/semper_viventem/backdropview/extensions.kt
+++ app/src/main/java/ru/semper_viventem/backdropview/extensions.kt
@@ -1,8 +1,10 @@
 package ru.semper_viventem.backdropview
 
+import android.content.Context
 import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.CoordinatorLayout.Behavior
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 
 fun <T : CoordinatorLayout.Behavior<*>> View.findBehavior(): T = layoutParams.run {
@@ -10,4 +12,13 @@ fun <T : CoordinatorLayout.Behavior<*>> View.findBehavior(): T = layoutParams.ru
 
     (layoutParams as CoordinatorLayout.LayoutParams).behavior as? T
             ?: throw IllegalArgumentException("Layout's behavior is not current behavior")
+}
+
+fun View.showKeyboard(show: Boolean) {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    if (show) {
+        if (requestFocus()) imm.showSoftInput(this, 0)
+    } else {
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
 }
